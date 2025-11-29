6d2f6116fd41a018a94690012fa5e836706ae960
Jorge Ruben Leos Sanchez <57677010+rubenleos@users.noreply.github.com>
2025-11-29 10:12:58 -0600

    Update enviroment.dart

--- lib/enviroment/enviroment.dart
+++ lib/enviroment/enviroment.dart
@@ -5,10 +5,11 @@ class Enviroment {
   // para evitar problemas de valores nulos durante el desarrollo en web.
   // Asegúrate de que esta sea la URL correcta de tu backend local.
   static const String API_URL = 'http://127.0.0.1:8000';
-  static const API_MERCADO_PAGO = 'https://api.mercadopago.com/v1';
-   static const ACCESS_TOKEN= "TEST-8924484007365875-061815-e993285aff913ff9710e37a95f77d064-72857723";
+  static const API_MERCADO_PAGO = '';
+   static const ACCESS_TOKEN= "";
 
-   static const String PUBLIC_KEY = 'TEST-51703793-b227-4e6a-9b45-ec7ee2055aa9';
+   static const String PUBLIC_KEY = '';
 }
+
 
 
